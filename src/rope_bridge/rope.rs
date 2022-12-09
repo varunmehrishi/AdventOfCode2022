0335e642e36e3b3d4b5337c695c072e1f1efd472
Varun Mehrishi <varunmehrishi@gmail.com>
2022-12-10 03:56:10 +0530

    Rename tail to knot

--- src/rope_bridge/rope.rs
+++ src/rope_bridge/rope.rs
@@ -24,7 +24,7 @@ impl<const N: usize> Rope<N> {
         self.knots[0].0 += dx;
         self.knots[0].1 += dy;
 
-        self.update_tail(1);
+        self.update_knot(1);
     }
 
     fn get_differentials(d: Direction) -> (i32, i32) {
@@ -36,33 +36,32 @@ impl<const N: usize> Rope<N> {
         }
     }
 
-    fn update_tail(&mut self, i: usize) {
+    fn update_knot(&mut self, i: usize) {
         if i == N {
             return;
         }
 
-        if self.is_tight(i) {
-            self.tail_x_update_dir(i)
+        if self.is_tight_at_knot(i) {
+            self.knot_x_update_dir(i)
                 .into_iter()
-                .chain(self.tail_y_update_dir(i).into_iter())
+                .chain(self.knot_y_update_dir(i).into_iter())
                 .map(Rope::<N>::get_differentials)
                 .for_each(|(dx, dy)| {
                     self.knots[i].0 += dx;
                     self.knots[i].1 += dy;
                 });
+            self.update_knot(i + 1)
         }
-
-        self.update_tail(i + 1)
     }
 
-    fn is_tight(&self, i: usize) -> bool {
+    fn is_tight_at_knot(&self, i: usize) -> bool {
         let (hx, hy) = self.knots[i - 1];
         let (tx, ty) = self.knots[i];
 
         (hx.abs_diff(tx) > 1) || (hy.abs_diff(ty) > 1)
     }
 
-    fn tail_x_update_dir(&self, i: usize) -> Option<Direction> {
+    fn knot_x_update_dir(&self, i: usize) -> Option<Direction> {
         match self.knots[i - 1].0.cmp(&self.knots[i].0) {
             Ordering::Less => Some(Direction::Up),
             Ordering::Equal => None,
@@ -70,7 +69,7 @@ impl<const N: usize> Rope<N> {
         }
     }
 
-    fn tail_y_update_dir(&self, i: usize) -> Option<Direction> {
+    fn knot_y_update_dir(&self, i: usize) -> Option<Direction> {
         match self.knots[i - 1].1.cmp(&self.knots[i].1) {
             Ordering::Less => Some(Direction::Left),
             Ordering::Equal => None,
