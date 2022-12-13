c054aaf0a5ec95197a6fc285662649f27630971c
Varun Mehrishi <varunmehrishi@gmail.com>
2022-12-13 21:41:10 +0530

    Dedup partial_cmp implementation

--- src/distress_signal/packet.rs
+++ src/distress_signal/packet.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::cmp::Ordering::{Greater, Less};
+use std::cmp::Ordering::{Greater, Less, self};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 #[serde(untagged)]
@@ -9,24 +9,13 @@ pub enum Packet {
 }
 
 impl PartialOrd for Packet {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        match (self, other) {
-            (Packet::Num(a), Packet::Num(b)) => a.partial_cmp(b),
-            (Packet::Num(a), Packet::Sub(vals)) => match vals.as_slice() {
-                [] => Some(Greater),
-                [b, ..] => Packet::Num(*a).partial_cmp(b),
-            },
-            (Packet::Sub(vals), Packet::Num(b)) => match vals.as_slice() {
-                [] => Some(Less),
-                [a, ..] => a.partial_cmp(&Packet::Num(*b)),
-            },
-            (Packet::Sub(vals_a), Packet::Sub(vals_b)) => vals_a.partial_cmp(vals_b),
-        }
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
 impl Ord for Packet {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+    fn cmp(&self, other: &Self) -> Ordering {
         match (self, other) {
             (Packet::Num(a), Packet::Num(b)) => a.cmp(b),
             (Packet::Num(a), Packet::Sub(vals)) => match vals.as_slice() {
